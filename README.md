853450e6dc34a20a1b8599b58403bdf007fb6c0c
cweibel <christopher.weibel@gmail.com>
2022-06-17 13:45:08 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # terraform-module-cf-ssh-lb
 Terraform to create the load balancer for CF SSH
+
+This module will spin a NLB for CF SSH.
+
+Inputs - Required:
+
+ - `resource_tags` - AWS tags to apply to resources
+ - `vpc_id` - AWS VPC Id
+ - `subnet_ids` - The AWS Subnet Id to place the lb into     
+ - `ssh_domain` - url used for cf ssh commands     
+ - `route53_zone_id` - Route53 zone id
+
+Inputs - Optional: 
+
+ - `enable_route_53` - Disable if using CloudFlare or other DNS (default = 1, to disable, set = 0)
+ - `internal_lb` - Determine whether the load balancer is internal-only facing (default = true)
+ 
+Outputs:
+
+ - `dns_name` - The A Record for the created load balancer
---
+++ main.tf
@@ -0,0 +1,75 @@
+variable subnet_ids            {}  # The AWS Subnet Id to place the lb into
+variable resource_tags         {}  # AWS tags to apply to resources
+variable vpc_id                {}  # The VPC Id
+variable ssh_domain            {}  # url used for cf ssh commands
+variable route53_zone_id       {}  # Route53 zone id
+variable internal_lb           { default = true } # Determine whether the load balancer is internal-only facing
+
+variable enable_route_53       { default = 1 }  # Disable if using CloudFlare or other DNS
+
+
+################################################################################
+# CF SSH NLB 
+################################################################################
+resource "aws_lb" "cf_ssh_nlb" {
+  name               = "cf-ssh-nlb"
+  internal           = var.internal_lb
+  load_balancer_type = "network"
+  subnets            = var.subnet_ids
+  # NOTE: NLB "network" LB does not support security groups.
+  tags               = merge(
+      {Name              = "cf-ssh-nlb"}, 
+      {Environment       = "cf-ssh" },
+      var.resource_tags
+  )
+}
+
+################################################################################
+# NLB Target Group
+################################################################################
+resource "aws_lb_target_group" "cf_ssh_nlb_tg" {
+  name         = "cf-ssh-nlb-tg"
+  port         = 2222
+  protocol     = "TCP"
+  vpc_id       = var.vpc_id
+  target_type  = "instance"
+  tags         = merge(
+      {Name              = "cf-ssh-nlb-tg"}, 
+      {Environment       = "cf-ssh" },
+      var.resource_tags
+  )
+}
+
+
+
+###############################################################################
+# NLB Listener for CF SSH
+###############################################################################
+resource "aws_lb_listener" "cf_ssh_nlb_listener_sys" {
+  load_balancer_arn = aws_lb.cf_ssh_nlb.arn
+  port = "2222"
+  protocol = "TCP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.cf_ssh_nlb_tg.arn
+  }
+
+  tags               = merge(
+      {Name              = "cf-ssh-nlb-listener-sys"}, 
+      {Environment       = "cf-ssh" },
+      var.resource_tags
+  )  
+}
+
+################################################################################
+# CF SSH NLB Route53 DNS CNAME Record - SSH Domain
+################################################################################
+resource "aws_route53_record" "cf_ssh_nlb_record_ssh" {
+  count   = var.enable_route_53
+  zone_id = var.route53_zone_id
+  name = var.ssh_domain
+  type = "CNAME"
+  ttl = "60"
+  records = ["${aws_lb.cf_ssh_nlb.dns_name}"]
+}
+
